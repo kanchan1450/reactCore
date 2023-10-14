56c28f0f2a3b7d5decc5412460b069031e5dc0e5
kanchan1450 <kanchan4596@gmail.com>
2023-10-14 11:55:30 +0530

    set up JSX to write code as code in react is tough to read

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"}, [
+// React.creatElement => React Elemnt - JS object => HTML Elemnt(render)
+const parent = React.createElement("div", {id: "parent"}, 
     React.createElement("div",{id: "child"},[
     React.createElement("h2",{},"I am in h2 tag"),
     React.createElement("h3", {}, "I am in h3 tag")
 
 
-]),
-    React.createElement("div",{id: "child2"},[
-    React.createElement("h2",{},"I am in h1 tag"),
-    React.createElement("h3", {}, "I am in h2 tag")]
-)]);
+]));
+console.log(parent);
+
+// JSX - it is a HTML like syntax, It is used to write development environment friendly code, bcoz react core code is very tough to read and maintain it.
+const heading = <h1 id = "heading">I am JSX</h1>
+console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
