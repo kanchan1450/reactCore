afbbc5b37981047d705f9735b4f1d515cdb551c6
kanchan1450 <kanchan4596@gmail.com>
2023-10-16 15:46:42 +0530

    food app created by static data

--- App.js
+++ App.js
@@ -1,33 +1,77 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
 
-const title = <h1 id="title">Learning React from basic</h1>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className = "logo" src="https://images-platform.99static.com/v84irzbNBd5aawXGKXfH4SEjcn0=/0x0:960x960/500x500/top/smart/99designs-contests-attachments/117/117132/attachment_117132760"></img> 
 
-const Component =() => {
-    return <h2>I am another function function component, which resite in another function</h2>
+            </div>
+            <div className="Nav-item">
+                <ul>
+                    <li>Home</li>
+                     <li>About Us</li>
+                     <li>Contact Us</li>
+                     <li>
+                        cart
+                     </li>
+                </ul>
+                
+            </div>
+        </div>
+    );
+};
+
+const RestoCard = () => {
+    return(
+        <div className="res-card">
+            <img className="res-logo"
+            alt = "res-logo"
+             src ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"></img>
+            <h3 className="resto-name">Mom's Kitchen</h3>
+            <h4> Biryani, North Indian, Asian</h4>
+            <h4>4.8</h4>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="resto-container">
+                <RestoCard resName ="Meghna Food" cuisine ="Biryani, North Indian, Asian"/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+                <RestoCard/>
+            </div>
+        </div>
+    )
 }
-// Creation of functional component in react
 
-const Heading = () => (
-    <div id="container">
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
 
-        {/* This is calling of React element inside it */}
-        {title} 
-
-
-        {/* we can directly write any string and mathematical opearation with the help of curly braces{} */}
-        {1000+2000}
-        {<h2>My name is kanchan sharma</h2>}
-
-        
-        {/* This is calling method of function inside another function */}
-        {Component()}  
-        <Component/> 
-        <Component></Component>
-    <h1 className="heading">This is functional component Learning</h1>
-    </div>
-);
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // rendering of functional component is used to do with angular bracket
-root.render(<Heading/>);
+root.render(<AppLayout/>)
