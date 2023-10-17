4011edc6b1b9d868a702d65f638884dc67ea6178
kanchan1450 <kanchan4596@gmail.com>
2023-10-17 16:43:43 +0530

    New feature added, which is on clicking button we will get list of top rated restaurnat.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,22 @@
 import RestoCard from "./RestoCard";
 import resList from "../utils/mockData";
+import {useState} from "react";
 const Body = () => {
+
+    const [listOfRestaurant, setistOfRestaurant] = useState(resList);
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn"
+                onClick={() => {
+                    const filterdlist = listOfRestaurant.filter (
+                        (res) => res.info.avgRating>4.2
+                    );
+                    setistOfRestaurant(filterdlist);
+                }}> Top Rated Restaurant</button>
+                </div>
             <div className="resto-container">
-                {resList.map((restaurant) => (
+                {listOfRestaurant.map((restaurant) => (
                 <RestoCard key = {restaurant.info.id} resData = {restaurant}/>))}
             
                 
